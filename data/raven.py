4a5a990b70d9b5c2898ae5801a00bba259511540
Lingxiao.Yang <zjjconan@gmail.com>
2024-07-26 17:21:10 +0800

    Update raven.py

--- data/raven.py
+++ data/raven.py
@@ -19,7 +19,7 @@ sub_folders = {'0': "center_single",
 class RAVEN(Dataset):
     def __init__(
         self, dataset_dir, data_split=None, image_size=80, 
-        transform=None, subset=None
+        transform=None, subset="None"
     ):
         self.dataset_dir = dataset_dir
         self.data_split = data_split
